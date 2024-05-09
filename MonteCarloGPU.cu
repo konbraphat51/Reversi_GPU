6b49ad4cd6230da2c2e201277cf21db1c945bb9c
konbraphat51 <konbraphat@gmail.com>
2024-05-10 06:18:39 +0900

    to 1-dim array

--- MonteCarloGPU.cu
+++ MonteCarloGPU.cu
@@ -5,5 +5,13 @@
 
 int mcGPU_move(BoardState *state)
 {
-
+    // convert BoardState to a format that can be used by the GPU
+    int board[BOARD_H * BOARD_W];
+    for (int x = 0; x < BOARD_W; x++)
+    {
+        for (int y = 0; y < BOARD_H; y++)
+        {
+            board[BOARD_W * y + x] = state->board[x][y];
+        }
+    }
 }
